eca4899b77d748532d3d3a05e3a2eb908e7a3921
kitta11 <parrag.krisztina@gmail.com>
2025-02-21 09:10:11 +0100

    feat/add mention

--- src/components/note.component.tsx
+++ src/components/note.component.tsx
@@ -2,19 +2,31 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import { NoteProps } from '../types'
 import debounce from 'lodash/debounce'
 import { useNotes } from '../hooks/use-notes'
+import { useUsers } from '../hooks/use-users'
 
 export function Note(props: NoteProps & { isNew?: boolean }) {
 	const divRef = useRef<HTMLDivElement>(null)
 	const caretPosRef = useRef<number>(0)
 
-	const [noteBodyText, setNoteBodyText] = useState(props.body || '')
+	const [noteBodyInnerHTML, setNoteBodyInnerHTML] = useState(props.body || '')
+
+	const [showMentionInput, setShowMentionInput] = useState(false)
+	const [mentionQuery, setMentionQuery] = useState('')
+	const [cursorPosition, setCursorPosition] = useState<number | null>(null)
+	const [cursorPositionX, setCursorPositionX] = useState<number | null>(null)
+	const [cursorPositionY, setCursorPositionY] = useState<number | null>(null)
+
 	const { updateNote, addNote } = useNotes()
 
+	const { data: usersData, isLoading: usersLoading } = useUsers()
+
 	const autoSave = () => {
+		if (showMentionInput) return
+
 		if (props.id !== undefined) {
-			updateNote(props.id, noteBodyText)
+			updateNote(props.id, noteBodyInnerHTML)
 		} else {
-			addNote(noteBodyText)
+			addNote(noteBodyInnerHTML)
 		}
 	}
 
@@ -22,13 +34,19 @@ export function Note(props: NoteProps & { isNew?: boolean }) {
 
 	useEffect(() => {
 		autoSaveRef.current = autoSave
-	}, [noteBodyText])
+	}, [noteBodyInnerHTML])
 
 	const handleDebouncedAutoSave = useMemo(() => {
 		const func = () => {
 			autoSaveRef.current?.()
 		}
-		return debounce(func, 1000)
+		return debounce(func, 1500)
+	}, [])
+
+	useEffect(() => {
+		if (divRef.current) {
+			divRef.current.innerHTML = props.body || ''
+		}
 	}, [])
 
 	function getCaretPosition(el: HTMLElement) {
@@ -41,6 +59,16 @@ export function Note(props: NoteProps & { isNew?: boolean }) {
 		preCaretRange.selectNodeContents(el)
 		preCaretRange.setEnd(range.endContainer, range.endOffset)
 		return preCaretRange.toString().length
+	}
+
+	function getCaretCoordinates(): { x: number; y: number } | null {
+		const selection = window.getSelection()
+		if (!selection || selection.rangeCount === 0) return null
+
+		const range = selection.getRangeAt(0)
+		const rect = range.getBoundingClientRect() // Get position
+
+		return { x: rect.left, y: rect.bottom - 28 }
 	}
 
 	function setCaretPosition(el: HTMLElement, position: number) {
@@ -79,8 +107,8 @@ export function Note(props: NoteProps & { isNew?: boolean }) {
 			caretPosRef.current = caretPos
 		}
 
-		const currentText = event.currentTarget.textContent || ''
-		setNoteBodyText(currentText)
+		const currentInnerHtml = event.currentTarget.innerHTML || ''
+		setNoteBodyInnerHTML(currentInnerHtml)
 
 		requestAnimationFrame(() => {
 			if (divRef.current) {
@@ -89,6 +117,118 @@ export function Note(props: NoteProps & { isNew?: boolean }) {
 		})
 
 		handleDebouncedAutoSave()
+	}
+
+	function handleMentions(event: React.KeyboardEvent<HTMLDivElement>) {
+		if (!divRef.current) return
+		event.stopPropagation()
+
+		if (event.key === '@') {
+			const caretPos = getCaretPosition(divRef.current) // Store caret
+			caretPosRef.current = caretPos
+			const caretCoordinates = getCaretCoordinates()
+			if (caretCoordinates) {
+				setCursorPositionX(caretCoordinates.x)
+				setCursorPositionY(caretCoordinates.y)
+			}
+			setShowMentionInput(true)
+			setCursorPosition(caretPos)
+			setMentionQuery('')
+
+			divRef.current.setAttribute('contenteditable', 'false')
+
+			event.preventDefault()
+		}
+	}
+
+	function renderFilteredUserList() {
+		const filteredUserList = usersData
+			.filter((user) => user.username.toLowerCase().includes(mentionQuery.toLowerCase()))
+			.slice(0, 5)
+		if (usersLoading || !usersData) {
+			return <li className="overlayListItem">Loading...</li>
+		}
+
+		if (filteredUserList.length === 0) {
+			return <li className="overlayListItem">No users found</li>
+		}
+
+		return filteredUserList.map((user) => (
+			<li
+				className="overlayListItem"
+				key={user.username}
+				onClick={() => insertMention(user.username, user.email)}
+			>
+				{user.username}
+			</li>
+		))
+	}
+
+	function setContentEditableActive() {
+		if (!divRef.current) return
+		setShowMentionInput(false)
+		setMentionQuery('')
+		divRef.current.setAttribute('contenteditable', 'true')
+		divRef.current.focus()
+		setCaretPosition(divRef.current, cursorPosition ?? 0)
+	}
+
+	function insertMention(username: string, mailto?: string) {
+		if (!divRef.current || !cursorPosition) return
+
+		// Create and style mention element
+		const mentionElement = document.createElement('a')
+		mentionElement.textContent = `@${username}`
+		if (mailto) mentionElement.href = `mailto:${mailto}`
+		mentionElement.classList.add('text-orange')
+		mentionElement.classList.add('cursor-pointer')
+		mentionElement.classList.add('hover:underline')
+		mentionElement.title = `email: ${mailto}`
+
+		// Re-enable contenteditable and set focus, reinstall cursor position
+		setContentEditableActive()
+
+		// Insert mention at the saved caret position
+		insertAtCaret(mentionElement)
+	}
+
+	function insertAtCaret(element: HTMLElement) {
+		const selection = window.getSelection()
+		const range = selection?.getRangeAt(0)
+		if (!range || !selection) return
+
+		range.deleteContents()
+		range.insertNode(element)
+
+		range.collapse(false)
+
+		// Create a new range to place the caret after the inserted mention element
+		const newRange = document.createRange()
+		newRange.setStartAfter(element)
+		newRange.setEndAfter(element)
+
+		// Remove any existing selections and add the new range
+		selection.removeAllRanges()
+		selection.addRange(newRange)
+
+		// Get the contenteditable element and update its state too
+		const editableElement = divRef.current
+
+		if (!editableElement) return
+
+		handleNoteBodyTextChange({
+			currentTarget: editableElement,
+		} as React.FormEvent<HTMLDivElement>)
+	}
+
+	function handleMentionInputKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+		if (event.key === 'Escape') {
+			setContentEditableActive()
+		}
+	}
+
+	function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+		setMentionQuery(event.target.value)
 	}
 
 	return (
@@ -103,9 +243,31 @@ export function Note(props: NoteProps & { isNew?: boolean }) {
 				onInput={handleNoteBodyTextChange}
 				suppressContentEditableWarning
 				className="bg-white w-full my-3 p-3"
-			>
-				{noteBodyText}
-			</div>
+				onKeyDown={handleMentions}
+			/>
+			{showMentionInput && (
+				<div
+					style={{
+						position: 'absolute',
+						top: cursorPositionY ?? 0 + window.scrollY, // Adjust for scrolling
+						left: cursorPositionX ?? 0 + window.scrollX,
+						background: 'white',
+						border: '1px solid var(--note-orange)',
+						borderRadius: '4px',
+						padding: 'var(--note-spacing-1)',
+					}}
+				>
+					<input
+						type="text"
+						value={mentionQuery}
+						onChange={handleInputChange}
+						onKeyDown={handleMentionInputKeyDown}
+						autoFocus
+						className="bg-orange text-white text-md p-1"
+					/>
+					<ul>{renderFilteredUserList()}</ul>
+				</div>
+			)}
 		</div>
 	)
 }
