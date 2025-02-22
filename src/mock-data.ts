3977f769270c9774933d8dd2617249b362388537
kitta11 <parrag.krisztina@gmail.com>
2025-02-22 15:35:43 +0100

    test note component

---
+++ src/mock-data.ts
@@ -0,0 +1,10 @@
+export const noteMockData1 = {
+	id: 1,
+	body: '',
+}
+export const noteMockData2 = {
+	id: 2,
+	body: 'Another todo item body',
+}
+
+export const notesMockData = [noteMockData1, noteMockData2]
---
+++ src/setupTests.ts
@@ -0,0 +1,3 @@
+import '@testing-library/jest-dom'
+//This will extend Jest's matchers with @testing-library/jest-dom's matchers (including toBeInTheDocument).
+console.log('SetupTests is running') // Add this line to confirm execution
